2ed87d231a60577335cf808686e21ddcc98d864e
ChannnXY <708812775@qq.com>
2019-11-18 10:14:59 +0800

    Feature(mianActivity):完成底部导航栏

--- app/src/main/java/com/channnxy/school_bus/MainActivity.java
+++ app/src/main/java/com/channnxy/school_bus/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.nav_bottom_index);
+        initBottomNavigation();
+    }
+
+    private void initBottomNavigation() {
+        mBottomNavigationView.setItemIconTintList(null);
     }
 }
